8cfc50ff1ca6f5165d3c8bf2174c4e07d2b9eaeb
Abhay S N <abhay182005@gmail.com>
2025-02-27 22:44:15 +0530

    Build the skeleton snake !

--- main.py
+++ main.py
@@ -33,6 +33,17 @@ while True:
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~BASICS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from pygame.math import Vector2
 import random
+
+class snake:
+    def __init__(self):
+        self.body = [Vector2(5,10), Vector2(6,10), Vector2(7,10)]
+    def draw_snake(self):
+        for block in self.body:
+            #create a rect and draw the rectangle Rect(x-pos, y-pos, width, height) *IMPORTANT*
+            xpos = int(block.x * cell_size)
+            ypos = int(block.y * cell_size)
+            block_rect = pygame.Rect(xpos, ypos, cell_size, cell_size)
+            pygame.draw.rect(screen, (183, 111, 122), block_rect)
 class Fruit:
     def __init__(self):
         self.x = random.randint(0,cell_number - 1) # create an x and y position also it includes the second parameter 
@@ -51,6 +62,7 @@ screen = pygame.display.set_mode((cell_number*cell_size, cell_number*cell_size))
 clock = pygame.time.Clock()
 
 fruit = Fruit()
+snake = snake() 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -58,5 +70,6 @@ while True:
             sys.exit()
     screen.fill((175, 215, 70))
     fruit.draw_fruit()
+    snake.draw_snake()
     pygame.display.update()
     clock.tick(60)
