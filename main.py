e1955fa011bb5cb48591e682d2d5b17bdb6369fe
Abhay S N <abhay182005@gmail.com>
2025-03-02 23:27:34 +0530

    Update

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ import random
 class snake:
     def __init__(self):
         self.body = [Vector2(5,10), Vector2(6,10), Vector2(7,10)]
+        self.direction = Vector2(1,0)
     def draw_snake(self):
         for block in self.body:
             #create a rect and draw the rectangle Rect(x-pos, y-pos, width, height) *IMPORTANT*
@@ -44,6 +45,12 @@ class snake:
             ypos = int(block.y * cell_size)
             block_rect = pygame.Rect(xpos, ypos, cell_size, cell_size)
             pygame.draw.rect(screen, (183, 111, 122), block_rect)
+
+    def move_snake(self):
+        bodycopy = self.body[:-1] # first element to the second last element
+        bodycopy.insert(0,bodycopy[0] + self.direction)
+        self.body = bodycopy[:]
+
 class Fruit:
     def __init__(self):
         self.x = random.randint(0,cell_number - 1) # create an x and y position also it includes the second parameter 
@@ -63,11 +70,27 @@ clock = pygame.time.Clock()
 
 fruit = Fruit()
 snake = snake() 
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE,150) # every 150 milliseconds the event will be triggered
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == SCREEN_UPDATE:
+            snake.move_snake()
+        if event.type == pygame.KEYDOWN: # check for specific keys (if you press any button)
+            if event.key == pygame.K_UP:
+                snake.direction = Vector2(0,-1)
+            if event.key == pygame.K_DOWN:
+                snake.direction = Vector2(0,1)
+            if event.key == pygame.K_LEFT:
+                snake.direction = Vector2(-1,0)
+            if event.key == pygame.K_RIGHT:
+                snake.direction = Vector2(1,0)
+                
+
+
     screen.fill((175, 215, 70))
     fruit.draw_fruit()
     snake.draw_snake()
